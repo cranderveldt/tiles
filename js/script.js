da4399e50df8c0e354717a84e075aa495196a57a
cranderveldt <djnaphouse@gmail.com>
2014-02-23 18:07:42 -0500

    TILES IS WORKING

--- js/script.js
+++ js/script.js
@@ -1,81 +1,64 @@
-var DND = {};
-DND.rotateClockwise = function(str) {
-  var num = parseInt(str, 10);
-  if (num === 270) {
-    return 0;
-  } else {
-    return num + 90;
-  }
-};
-jQuery(document).ready(function($) {
-  DND.initialize = function(){
+/*jQuery(document).ready(function(){
+  
+});*/
+var Main = function ($scope) {
+  $scope.maps = [{ name: 'Default', tiles: [] }];
+  $.cookie.json = true;
+  $scope.alltiles = [
+    { name: "Left Turn", num: 1 }
+    , { name: "Right Turn", num: 2 }
+    , { name: "Straight", num: 3 }
+    , { name: "T", num: 4 }
+    , { name: "4 Corners", num: 5 }
+    , { name: "2 Corners", num: 6 }
+    , { name: "Pong Left", num: 7 }
+    , { name: "Pong Right", num: 8 }
+    , { name: "One wall", num: 9 }
+    , { name: "Two walls", num: 10 }
+    , { name: "Three Walls", num: 11 }
+    , { name: "White", num: 12 }
+    , { name: "Black", num: 13 }
+  ];
+  $scope.initialize = function() {
     var cookie = $.cookie('tiles');
     if (cookie !== undefined) {
-
+      $scope.maps = cookie;
+    }
+    
+  };
+  $scope.rotateClockwise = function(str) {
+    var num = parseInt(str, 10);
+    if (num === 270) {
+      return 0;
     } else {
-
+      return num + 90;
     }
   };
-  $('#tiles .tile').draggable({
-    grid: [100, 100]
-  });
-  DND.initialize();
-});
-
-var Main = function ($scope) {
-  $scope.maps = [
-    {
-      name: 'Default'
-      , tiles: [
-        {
-          src: '1'
-          , rotate: '0'
-        }
-        , {
-          src: '2'
-          , rotate: '0'
-        }
-        , {
-          src: '3'
-          , rotate: '0'
-        }
-        , {
-          src: '4'
-          , rotate: '0'
-        }
-        , {
-          src: '5'
-          , rotate: '0'
-        }
-        , {
-          src: '6'
-          , rotate: '0'
-        }
-        , {
-          src: '7'
-          , rotate: '0'
-        }
-        , {
-          src: '8'
-          , rotate: '0'
-        }
-        , {
-          src: '9'
-          , rotate: '0'
-        }
-        , {
-          src: '10'
-          , rotate: '0'
-        }
-        , {
-          src: '11'
-          , rotate: '0'
-        }
-      ]
+  
+  $scope.saveMap = function() {
+    $.cookie('tiles', $scope.maps, { expires: 30, path: '/' });
+  };
+  $scope.nextSource = function(src) {
+    if (src === $scope.alltiles.length) {
+      return 1;
+    } else {
+      return src + 1;
     }
-  ]
+  };
+  $scope.addTile = function() {
+    $scope.$apply(function() {
+      $scope.maps[0].tiles.push({
+        src: '1', rotate: '0'
+      });
+      $scope.saveMap();
+    });
+  };
+  $scope.sortableOptions = {
+    items: "> .tile"
+  };
+  $scope.initialize();
 };
-var app = angular.module('dnd-tiles', []);
+var app = angular.module('dnd-tiles', ['ui.sortable']);
 app.controller('Main',['$scope','$http', Main]);
 app.directive('tlRotate', function () {
   return {
@@ -85,8 +68,55 @@ app.directive('tlRotate', function () {
     scope: false,
     link: function ($scope, element, attrs) {
       element.on('click', function(e) {
-        var degree = DND.rotateClockwise(attrs.tlRotate);
-        element.removeClass().addClass('rotate' + degree + 'deg');
+        var tileIndex = $('.tile').index(element.parent());
+        $scope.$apply(function() {
+          $scope.maps[0].tiles[tileIndex].rotate = $scope.rotateClockwise(attrs.tlRotate);
+          $scope.saveMap();
+        });
+      });
+    }
+  };
+});
+app.directive('tlSource', function () {
+  return {
+    replace: false,
+    transclude: false,
+    restrict: 'A',
+    scope: false,
+    link: function ($scope, element, attrs) {
+      element.on('change', function(e) {
+        var tileIndex = $('.tile').index(element.parent().parent());
+        $scope.$apply(function() {
+          $scope.maps[0].tiles[tileIndex].src = element.val();
+          $scope.saveMap();
+        });
+      });
+    }
+  };
+});
+app.directive('tlAddTile', function () {
+  return {
+    replace: false,
+    transclude: false,
+    restrict: 'A',
+    scope: false,
+    link: function ($scope, element, attrs) {
+      element.on('click', function(e) {
+        $scope.addTile();
+        $('#tiles').sortable("refresh");
+      });
+    }
+  };
+});
+app.directive('tlSortable', function () {
+  return {
+    replace: false,
+    transclude: false,
+    restrict: 'A',
+    scope: false,
+    link: function ($scope, element, attrs) {
+      element.on('sortchange', function(e) {
+        $scope.saveMap();
       });
     }
   };
